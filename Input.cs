4a8d0b9e3502d537c6ade53d5a47093e0ebb0e85
Chumak202 <zhenya.chumakov.02@mail.ru>
2023-12-28 15:37:10 +0300

    firs commit

---
+++ Input.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Input
+{
+    private static byte dir;
+    public static void Press(Direction dir)
+    {
+        Input.dir |= (byte)dir;
+    }
+    public static void Release(Direction dir)
+    {
+        Input.dir &= (byte)~(int)dir;
+    }
+    // pressed = true, released = false
+    public static bool GetState(Direction direction)
+    {
+        return (Input.dir & (byte)direction) != 0;
+    }
+}
+
+public enum Direction { Up = 8, Down = 1, Left = 2, Right = 4 }
+
+public readonly struct Tile
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Tile(int x, int y) { X = x; Y = y; }
+}
+
+
+class Program
+{
+    static PlayerMovement player;
+    private static void TestEquality(Direction direction, int x, int y)
+    {
+        player.Update();
+
+        if (player.Direction != direction)
+        {
+            throw new Exception($"Wrong direction! Should be {direction}, but was {player.Direction}");
+        }
+        if (x != player.Position.X)
+        {
+            throw new Exception($"Wrong position! Should be {x}, but was {player.Position.X}");
+        }
+        if (y != player.Position.Y)
+        {
+            throw new Exception($"Wrong position! Should be {y}, but was {player.Position.Y}");
+        }
+    }
+    public static void Main()
+    {
+        player = new PlayerMovement(0, 0);
+        Input.Press(Direction.Down);
+
+        TestEquality(Direction.Down, 0, 0);
+        TestEquality(Direction.Down, 0, -1);
+        TestEquality(Direction.Down, 0, -2);
+
+        Input.Press(Direction.Left);
+        Input.Press(Direction.Right);
+
+        TestEquality(Direction.Left, 0, -2);
+        TestEquality(Direction.Left, -1, -2);
+
+        Input.Release(Direction.Left);
+
+        TestEquality(Direction.Right, -1, -2);
+
+        Input.Release(Direction.Right);
+
+        TestEquality(Direction.Down, -1, -2);
+        TestEquality(Direction.Down, -1, -3);
+
+        Input.Release(Direction.Down);
+
+        TestEquality(Direction.Down, -1, -3);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static Program;
+
+
+public class PlayerMovement
+{
+    private bool isUp;
+    private bool isDown;
+    private bool isLeft;
+    private bool isRight;
+    private List<int> steps = new List<int>();
+    int _x = 0;
+    int _y = 0;
+    public Tile Position { get; private set; }
+    public Direction Direction { get; private set; }
+
+    public PlayerMovement(int x, int y)
+    {
+        int _x = 0;
+        if (steps.Count != 0)
+        {
+            Update();
+            List<int> step = new List<int>();
+            if (isRight && !steps.Contains(6)) step.Add(6);
+
+            if (isLeft && !steps.Contains(4)) step.Add(4);
+
+            if (isDown && !steps.Contains(2)) step.Add(2);
+
+            if (isUp && !steps.Contains(8)) step.Add(8);
+
+            foreach (int i in step)
+            {
+                if (step.Contains(steps[steps.Count -1]))
+                {
+                    if(steps.Count - 1 == 8)
+                    {
+                        _y += 1;
+                    }
+                    if (steps.Count - 1 == 2)
+                    {
+                        _y -= 1;
+                    }
+                    if(steps.Count - 1 == 4)
+                    {
+                        _x -= 1;
+                    }
+                    if (steps.Count - 1 == 6)
+                    {
+                        _x += 1;
+                    }
+                }
+                else
+                {
+                    steps = step;
+                }
+
+            }
+        }
+        else
+        {
+            try
+            {
+                Update();
+
+                if (isRight) steps.Add(6);
+
+                if (isLeft) steps.Add(4);
+
+                if (isDown) steps.Add(2);
+
+                if (isUp) steps.Add(8);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+
+    public void Update()
+    {
+        isUp = Input.GetState(Direction.Up);
+        isDown = Input.GetState(Direction.Down);
+        isLeft = Input.GetState(Direction.Left);
+        isRight = Input.GetState(Direction.Right);
+
+    }
+}
